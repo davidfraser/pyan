9032dea39dabf9ed2cd01bdfd2897ccdcad2e422
edmund <edmund@0f5934c0-91fa-694a-86a9-32a2c3e025ff>
2011-10-11 05:33:46 +0000

    Fix parallel mode to actually use more than one processor...

--- fractal/parallel.c
+++ fractal/parallel.c
@@ -1,9 +1,10 @@
 #include "fractal.h"
 
+#include <string.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-extern int omp_get_num_threads();
+extern int omp_get_num_procs();
 
 static int frame;
 static int num_frames;
@@ -17,7 +18,7 @@ void parallel_init(int w, int h)
 	frame = 0;
 	num_frames = 43;
 	frame_offset = 0;
-	num_jobs = omp_get_num_threads();
+	num_jobs = omp_get_num_procs();
 	width = w;
 	height = h;
 }
@@ -35,6 +36,10 @@ void parallel_update(void)
 	int num_pixels = width*height;
 	int pixels_per_job = (int) ceil((double) num_pixels / num_frames / num_jobs);
 	int j;
+    int old_pixels_done = pixels_done;
+    int thread_done[16];
+    
+    memset(thread_done, 0, sizeof(thread_done));
 
 	if (frame >= num_frames)
 		return;
@@ -52,9 +57,13 @@ void parallel_update(void)
 				int y = a / width;
 
 				do_pixel(x, y);
+                thread_done[j]++;
 			}
 		}
 	}
+    pixels_done = old_pixels_done;
+	for (j = 0; j < num_jobs; j++)
+        pixels_done += thread_done[j];
 
 	frame++;
 }
