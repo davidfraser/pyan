4fe34d7a75866b8f5c228e824d1297421e1bca9b
edmund <edmund@0f5934c0-91fa-694a-86a9-32a2c3e025ff>
2011-10-11 09:11:14 +0000

    Make a tag for another download, after implementing SIMD.

--- compiler/examples/if.c
+++
@@ -1,8 +0,0 @@
-int main(void)
-{
-    printf("if1(1) = %d\n", if1(1));
-    printf("if1(2) = %d\n", if1(2));
-    printf("if2(1) = %d\n", if2(1));
-    printf("if2(2) = %d\n", if2(2));
-    return 0;
-}
