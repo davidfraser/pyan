5c93c5bedd578b2749aa247524e74321fa659ac4
ejrh00 <ejrh00@0f5934c0-91fa-694a-86a9-32a2c3e025ff>
2012-09-02 09:21:20 +0000

    Write enough of the sender and receiver to learn that having them as separate programs binding to the same port may not work out.

--- snmp/receiver.c
+++ snmp/receiver.c
@@ -7,12 +7,20 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <sys/fcntl.h>
+#include <errno.h>
 
 #include "snmp.h"
 
 #define BUFLEN 65535
-#define NPACK 10
-#define PORT 12345
+
+#define DEFAULT_LISTEN_PORT 12345
+
+typedef struct Options
+{
+    int verbose;
+    int listen_port;
+} Options;
 
 void diep(char *s)
 {
@@ -20,37 +28,106 @@ void diep(char *s)
     exit(1);
 }
 
-int main(void)
+static void parse_args(int argc, char *argv[], Options *options)
+{
+    int c;
+    
+    options->verbose = 0;
+    options->listen_port = DEFAULT_LISTEN_PORT;
+
+    opterr = 1;
+
+    while ((c = getopt (argc, argv, "vp:")) != -1)
+        switch (c)
+        {
+            case 'v':
+                options->verbose = 1;
+                break;
+            case 'p':
+                options->listen_port = strtol(optarg, NULL, 0);
+                break;
+            default:
+                exit(1);
+        }
+
+    if (options->listen_port < 1 || options->listen_port > 65535)
+    {
+        fprintf(stderr, "Listen port must be between 1 and 65535\n");
+        exit(1);
+    }
+}
+
+static void log_message(SNMPMessage *message)
+{
+    char *host_str = "host";
+    char *timestamp_str = "timestamp";
+    char *oid_str;
+    char *value_str;
+    int i = 0;
+    
+    while (snmp_get_varbind(message, i, &oid_str, &value_str))
+    {
+        printf("%s\t%s\t%s\t%s\n", host_str, timestamp_str, oid_str, value_str);
+        i++;
+    }
+}
+
+static void run(Options *options)
 {
     struct sockaddr_in si_me, si_other;
-    int s, i, slen = sizeof(si_other);
+    int s;
+    socklen_t slen = sizeof(si_other);
     char buf[BUFLEN];
+    int reuse = 1;
 
     if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
         diep("socket");
+    
+    if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) != 0)
+        diep("setsockopt");
 
     memset((char *) &si_me, 0, sizeof(si_me));
     si_me.sin_family = AF_INET;
-    si_me.sin_port = htons(PORT);
+    si_me.sin_port = htons(options->listen_port);
     si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(s, &si_me, sizeof(si_me)) == -1)
+    if (bind(s, (struct sockaddr *) &si_me, sizeof(si_me)) == -1)
         diep("bind");
+    
+    if (options->verbose)
+        fprintf(stderr, "Listening on port %d\n", options->listen_port);
 
-    for (i = 0; i < NPACK; i++)
+    while (1)
     {
         SNMPMessage *message;
         int nr;
         
-        if ((nr = recvfrom(s, buf, BUFLEN, 0, &si_other, &slen)) == -1)
-            diep("recvfrom()");
-        printf("Received packet from %s:%d\n", 
-            inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+        nr = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen);
+        if (nr == -1)
+            diep("recvfrom");
+        
+        if (options->verbose)
+            fprintf(stderr, "Received packet from %s:%d\n", 
+                    inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
         
         message = snmp_parse_message(buf, nr);
-        snmp_print_message(message);
+        
+        if (options->verbose)
+            snmp_print_message(message, stderr);
+        
+        if (snmp_get_pdu_type(message) == SNMP_GET_RESPONSE_TYPE)
+            log_message(message);
+        
         snmp_destroy_message(message);
     }
 
     close(s);
+}
+
+int main(int argc, char *argv[])
+{
+    Options options;
+    
+    parse_args(argc, argv, &options);
+    run(&options);
     return 0;
 }
--- snmp/sender.c
+++ snmp/sender.c
@@ -7,13 +7,21 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <netdb.h>
 
 #include "snmp.h"
 
-#define SRV_IP "127.0.0.1"
+#define DEFAULT_LISTEN_PORT 12345
 
-#define NPACK 10
-#define PORT 12345
+#define DEFAULT_AGENT_PORT 161
+
+typedef struct Options
+{
+    int verbose;
+    int listen_port;
+    char *agent_host;
+    int agent_port;
+} Options;
 
 void diep(char *s)
 {
@@ -21,54 +29,140 @@ void diep(char *s)
     exit(1);
 }
 
-int main(void)
+static int split_host_port(char *input, int default_port, char **host, int *port)
 {
-    struct sockaddr_in si_other;
-    int s, i, slen = sizeof(si_other);
+    char *p;
+    
+    *host = strdup(input);
+    
+    if ((p = strchr(*host, ':')))
+    {
+        *port = strtol(p+1, NULL, 0);
+        *p = 0;
+    }
+    else
+    {
+        *port = default_port;
+    }
+    
+    return 1;
+}
 
+static void parse_args(int argc, char *argv[], Options *options)
+{
+    int c;
+    
+    options->verbose = 0;
+    options->listen_port = DEFAULT_LISTEN_PORT;
+
+    opterr = 1;
+
+    while ((c = getopt (argc, argv, "vp:")) != -1)
+        switch (c)
+        {
+            case 'v':
+                options->verbose = 1;
+                break;
+            case 'p':
+                options->listen_port = strtol(optarg, NULL, 0);
+                break;
+            default:
+                exit(1);
+        }
+
+    if (options->listen_port < 1 || options->listen_port > 65535)
+    {
+        fprintf(stderr, "Listen port must be between 1 and 65535\n");
+        exit(1);
+    }
+    
+    if (optind >= argc)
+    {
+        fprintf(stderr, "Need an agent host\n");
+        exit(1);
+    }
+    else if (!split_host_port(argv[optind], DEFAULT_AGENT_PORT, &options->agent_host, &options->agent_port))
+    {
+        fprintf(stderr, "Agent host cannot be parsed\n");
+        exit(1);
+    }
+}
+
+void run(Options *options)
+{
+    struct sockaddr_in si_me, si_other;
+    int s;
+    socklen_t slen = sizeof(si_other);
+    int reuse = 1;
+    struct hostent *he;
+    unsigned int next_request_id = 0;
+    
     if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
         diep("socket");
 
+    if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) != 0)
+        diep("setsockopt");
+
+    memset((char *) &si_me, 0, sizeof(si_me));
+    si_me.sin_family = AF_INET;
+    si_me.sin_port = htons(options->listen_port);
+    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+    
+    if (bind(s, (struct sockaddr *) &si_me, sizeof(si_me)) != 0)
+        diep("bind");
+
+    if (options->verbose)
+        fprintf(stderr, "Opened socket on port %d\n", options->listen_port);
+
     memset((char *) &si_other, 0, sizeof(si_other));
     si_other.sin_family = AF_INET;
-    si_other.sin_port = htons(PORT);
+    si_other.sin_port = htons(options->agent_port);
     
-    if (inet_aton(SRV_IP, &si_other.sin_addr) == 0) {
-        fprintf(stderr, "inet_aton() failed\n");
-        exit(1);
-    }
-
-    for (i = 0; i < NPACK; i++)
+    if (!(he = gethostbyname2(options->agent_host, AF_INET)))
+        diep("gethostbyname2");
+    
+    memmove(&si_other.sin_addr.s_addr, he->h_addr, he->h_length);
+    
+    while (1)
     {
         SNMPMessage *message;
         int len;
         unsigned char *buf;
       
-        printf("Generating message\n");
-        
         message = snmp_create_message();
         snmp_set_version(message, 0);
-        snmp_set_community(message, "private");
+        snmp_set_community(message, "public");
         snmp_set_pdu_type(message, SNMP_GET_REQUEST_TYPE);
-        snmp_set_request_id(message, i);
+        snmp_set_request_id(message, next_request_id++);
         snmp_set_error(message, 0);
         snmp_set_error_index(message, 0);
-        snmp_add_varbind_null(message, "1.3.6.1.4.1.2680.1.2.7.3.2.0");
+        snmp_add_varbind_null(message, "1.3.6.1.2.1.1.5.0");
         
         len = snmp_message_length(message);
         buf = malloc(len);
         snmp_render_message(message, buf);
         
-        snmp_print_message(message);
+        if (options->verbose)
+            snmp_print_message(message, stderr);
+        
         snmp_destroy_message(message);
         
-        printf("Sending packet %d\n", i);
-        if (sendto(s, buf, len, 0, &si_other, slen) == -1)
-            diep("sendto()");
+        if (options->verbose)
+            fprintf(stderr, "Sending datagram to %s:%d\n", options->agent_host, options->agent_port);
+        
+        if (sendto(s, buf, len, 0, (struct sockaddr *) &si_other, slen) == -1)
+            diep("sendto");
         
         free(buf);
     }
-
     close(s);
+}
+
+int main(int argc, char *argv[])
+{
+    Options options;
+    
+    parse_args(argc, argv, &options);
+    run(&options);
     return 0;
 }
