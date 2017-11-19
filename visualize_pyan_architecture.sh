4e5fabb80411067d448cd3b0fb94c44db5ae88b0
Juha Jeronen <juha.jeronen@jyu.fi>
2017-11-19 02:40:02 +0200

    visualize_pyan_architecture.sh: change the options to get one clear graph of everything

--- visualize_pyan_architecture.sh
+++ visualize_pyan_architecture.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-./pyan.py pyan/*.py --no-uses --defines --grouped --nested-groups --colored --dot --annotated >defines.dot
-./pyan.py pyan/*.py --uses --no-defines --grouped --nested-groups --colored --dot --annotated >uses.dot
-dot -Tsvg defines.dot >defines.svg
-dot -Tsvg uses.dot >uses.svg
-echo -ne "Pyan architecture: generated defines.svg and uses.svg\n"
+echo -ne "Pyan architecture: generating architecture.{dot,svg}\n"
+./pyan.py pyan/*.py --no-defines --uses --colored --annotate --dot -V >architecture.dot 2>architecture.log
+dot -Tsvg architecture.dot >architecture.svg
