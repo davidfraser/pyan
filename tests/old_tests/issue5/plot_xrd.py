eb63f543e7d19faa7b23cc7fc2a936dbce1c0e81
Juha Jeronen <juha.m.jeronen@gmail.com>
2020-12-10 20:16:47 +0200

    archive some old tests (issue #5)

---
+++ tests/old_tests/issue5/plot_xrd.py
@@ -0,0 +1,25 @@
+import plotly.offline as py
+import plotly.graph_objs as go
+
+from . import meas_xrd
+
+def plot_xrd(meas: meas_xrd.MeasXRD):
+    trace = go.Scatter(
+        x=meas.data["Angle"],
+        y=meas.data["Intensity"]
+    )
+
+    layout = go.Layout(
+        title="XRD data",
+        xaxis=dict(
+            title="Angle"
+        ),
+        yaxis=dict(
+            title="Intensity",
+            type="log"
+        )
+    )
+
+    data = [trace]
+    fig = go.Figure(data=data, layout=layout)
+    return py.plot(fig, output_type="div", include_plotlyjs=False)
---
+++ tests/old_tests/issue5/relimport.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8; -*-
+# See issue #5
+
+from .mod2 import foo
+from ..mod3 import bar
+from . import mod1
+from . import mod1 as moo
