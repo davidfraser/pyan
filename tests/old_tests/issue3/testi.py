21417f04e69f76793dd1a015d8da9a1afde4b589
Jan Beitner <beitner.jan@bcg.com>
2021-01-14 14:12:56 +0000

    make valid function h in old tests

--- tests/old_tests/issue3/testi.py
+++ tests/old_tests/issue3/testi.py
@@ -10,12 +10,12 @@ def g():
     return [(x, y) for x in range(10) for y in range(10)]
 
 
-# def h():
-#     return [
-#         (
-#             [(name, allargs) for name, _, _, allargs, _ in recs],
-#             {name: inargs for name, inargs, _, _, _ in recs},
-#             {name: meta for name, _, _, _, meta in recs},
-#         )
-#         for recs in (results[key] for key in sorted(results.keys()))
-#     ]
+def h(results):
+    return [
+        (
+            [(name, allargs) for name, _, _, allargs, _ in recs],
+            {name: inargs for name, inargs, _, _, _ in recs},
+            {name: meta for name, _, _, _, meta in recs},
+        )
+        for recs in (results[key] for key in sorted(results.keys()))
+    ]
